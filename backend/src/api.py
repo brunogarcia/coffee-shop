33acdfd46913e2f5de5291f995a5043328e3486f
Bruno Garcia <bruno@garciaechegaray.com>
2020-06-09 20:01:54 +0200

    feat: implement error handlers

--- backend/src/api.py
+++ backend/src/api.py
@@ -128,7 +128,10 @@ def create_drink(payload):
         it should update the corresponding row for <id>
         it should require the 'patch:drinks' permission
         it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
+    returns
+        status code 200
+        json {"success": True, "drinks": drink}
+        where drink an array containing only the updated drink
         or appropriate status code indicating reason for failure
 '''
 
@@ -140,41 +143,66 @@ def create_drink(payload):
         it should respond with a 404 error if <id> is not found
         it should delete the corresponding row for <id>
         it should require the 'delete:drinks' permission
-    returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
+    returns
+        status code 200
+        json {"success": True, "delete": id}
+        where id is the id of the deleted record
         or appropriate status code indicating reason for failure
 '''
 
 
-## Error Handling
-'''
-Example error handling for unprocessable entity
-'''
-@app.errorhandler(422)
-def unprocessable(error):
+@app.errorhandler(400)
+def bad_request():
+    '''
+    Handler for bad request
+    '''
     return jsonify({
-                    "success": False, 
-                    "error": 422,
-                    "message": "unprocessable"
-                    }), 422
+        "success": False,
+        "error": 400,
+        "message": "bad request"
+    }), 400
 
-'''
-@TODO implement error handlers using the @app.errorhandler(error) decorator
-    each error handler should return (with approprate messages):
-             jsonify({
-                    "success": False, 
-                    "error": 404,
-                    "message": "resource not found"
-                    }), 404
+@app.errorhandler(404)
+def not_found():
+    '''
+    Handler for resource not found
+    '''
+    return jsonify({
+        "success": False,
+        "error": 404,
+        "message": "resource not found"
+    }), 404
 
-'''
+@app.errorhandler(422)
+def unprocessable():
+    '''
+    Handler for unprocessable entity
+    '''
+    return jsonify({
+        "success": False,
+        "error": 422,
+        "message": "unprocessable entity"
+    }), 422
 
-'''
-@TODO implement error handler for 404
-    error handler should conform to general task above 
-'''
+@app.errorhandler(500)
+def internal_server_error():
+    '''
+    Handler for internal server error
+    '''
+    return jsonify({
+        "success": False,
+        "error": 500,
+        "message": "internal server error"
+    }), 500
 
-
-'''
-@TODO implement error handler for AuthError
-    error handler should conform to general task above 
-'''
+@app.errorhandler(AuthError)
+def auth_error(exception):
+    '''
+    Handler for AuthError
+    '''
+    return jsonify({
+        "success": False,
+        "error": exception.status_code,
+        "code": exception.error['code'],
+        "message": exception.error['description']
+    }), exception.status_code
